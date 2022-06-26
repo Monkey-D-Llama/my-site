35c08ad6a178833888fff49701ff92d16ae98555
Monkey-D-Llama <billy.ballsack69420@gmail.com>
2022-06-26 17:33:15 -0400

    finished all but games/apps

--- src/script.js
+++ src/script.js
@@ -51,7 +51,7 @@ const dispAbout = (ele) => {
     p.innerHTML = "Thanks for stopping by my site, I am Liam O'Brien.  " + 
     "I am a husband, father, programmer, gamer, " +
      "and climber with a passion pentesting." + 
-    "I am also a huge foodie and coffee addict with a real love for cooking";
+    "I am also a massive foodie and coffee connoisseur with a real love for cooking";
     ele.appendChild(p);
     ele.appendChild(document.createElement('div'));
     a = document.createElement('a');
@@ -78,7 +78,133 @@ const dispAbout = (ele) => {
 };
 
 const dispResume = (ele) => {
+    ele.innerHTML = '';
+    ele.className = 'resume';
+    let div = document.createElement('div')
+    let h1 = document.createElement('h1');
+    h1.innerHTML = 'Resume';
+    div.appendChild(h1);
+    ele.appendChild(div);
+    let h3 = document.createElement('h3');
+    h3.innerHTML = 'OVERVIEW';
+    ele.appendChild(h3);
+    let a = document.createElement('a');
+    a.innerHTML = 'Quick learner with a technical aptitude toward hardware, networking, machinery and software.  '+
+    'Skilled in problem solving and comfortable working with others and individually.  '+
+    'Willing to work evenings, weekends and extended hours as needed.';
+    ele.appendChild(a);
+    ele.appendChild(document.createElement('div'));
+    h3 = document.createElement('h3');
+    h3.innerHTML = 'EXPERIENCE';
+    ele.appendChild(h3);
+    div = document.createElement('div');
+    a = document.createElement('a');
+    a.innerHTML = '<strong>Lincoln Financial, Radnor, PA </strong>'+
+    '- Support Desk Technician';
+    div.appendChild(a);
+
+
+    let list = document.createElement('ul');
+    let li = document.createElement('li');
+    li.innerHTML = 'First line of support for customers experiencing technical '+
+    'issues accessing or using their on-line account';
+    list.appendChild(li);
+
+
+    li = document.createElement('li');
+    li.innerHTML = 'Adept at conversing with customers with varying levels of knowledge '+
+    'to assist them in a positive and supportive manner';
+    list.appendChild(li);
+
+    li = document.createElement('li');
+    li.innerHTML = 'Working with third party brokers to establish online accounts';
+    list.appendChild(li);
+
+    li = document.createElement('li');
+    li.innerHTML = 'ADT, ARDIS, WFM, JIRA, Converge3';
+    list.appendChild(li);
+
+    div.appendChild(list);
+    ele.appendChild(div);
+
+    div = document.createElement('div');
+
+    a = document.createElement('a');
+    a.innerHTML = '<strong>Mutual Benefit Group, Huntingdon, PA </strong>'+
+    '- .NET web developer';
+    div.appendChild(a);
+
+    list = document.createElement('ul');
+
+
+    li = document.createElement('li');
+    li.innerHTML = 'Seasonal employment over four consecutive summers, and one winter';
+    list.appendChild(li);
+
+    li = document.createElement('li');
+    li.innerHTML = 'Translating company website backend from Visual Basic to C#';
+    list.appendChild(li);
+
+    li = document.createElement('li');
+    li.innerHTML = 'Employee web page developement, created a gui for employees to interact with SQL and AS400 information';
+    list.appendChild(li);
     
+    li = document.createElement('li');
+    li.innerHTML = 'Design, coding and testing of api for data coming in from autoinsurance companies '+
+    'to internal SQL databases that we were tasked with maintaining';
+    list.appendChild(li);
+
+    li = document.createElement('li');
+    li.innerHTML = 'Developed a web page for insurance agents to access and edit insurance files using HTML forms';
+    list.appendChild(li);
+
+    li = document.createElement('li');
+    li.innerHTML = 'Training and support of end users on systems I developed';
+    list.appendChild(li);
+
+    li = document.createElement('li');
+    li.innerHTML = 'Reviewing and presenting options and giving recommendations for solutions when '+
+    'given a project or task, including demonstrating advantages and disadvantages of options set before us';
+    list.appendChild(li);
+
+
+    div.appendChild(list);
+    ele.appendChild(div);
+
+    h3 = document.createElement('h3');
+    h3.innerHTML = 'EDUCATION';
+    ele.appendChild(h3);
+
+
+    div = document.createElement('div');
+    a = document.createElement('a');
+    a.innerHTML = '<strong>Temple University, Philadelphia PA </strong>'+
+    '- College of Science and Technology';
+
+    div.appendChild(a);
+    list = document.createElement('ul');
+    li = document.createElement('li');
+    li.innerHTML = 'Completed 86 credits as part-time student towars a '+
+    'Bachelor of Science in Information Technology and Science '+
+    'with a focus on cyber security.';
+    list.appendChild(li);
+    div.appendChild(list);
+    ele.appendChild(div);
+
+
+    h3 = document.createElement('h3');
+    h3.innerHTML = 'TECHNICAL SKILLS';
+    ele.appendChild(h3);
+    
+
+    div = document.createElement('div');
+    //create the table here (gonna be fun)
+    ele.appendChild(div);
+
+    a = document.createElement('a');
+    a.href = './src/LO Resume.pdf';
+    a.innerHTML = 'download pdf';
+    ele.appendChild(a);
 };
 
 const dispContactInfo = (ele) => {
